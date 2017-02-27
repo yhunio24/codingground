dcb94d5abddffc339fc91804aaa05d4dd1ebd5ec
yhunio24 <yhunio62@gmail.com>
2017-02-27 09:23:39 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas2.java
@@ -0,0 +1,12 @@
+public class Tugas2{
+
+     public static void main(String []args){
+        String[] names= new String[]{ 
+            "Yhunio", "Miki", "Husna" 
+        };
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        
+    }
+}
