8476eddbec56e6eb200db64e71e139183c9eb988
yhunio24 <yhunio62@gmail.com>
2017-02-27 09:44:21 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas3.java
@@ -0,0 +1,14 @@
+public class Tugas3{
+    
+     public static void main(String []args) {
+        int num = 3;
+        int tampil = TampilAngka(num);
+        System.out.println("Angka yang anda masukkan adalah " + tampil);
+    }
+    
+     public static int TampilAngka(int num1) {
+      int angka = num1;
+      
+      return angka; 
+   }
+}
