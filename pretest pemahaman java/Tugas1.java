af050827f18378ea1d55908cc4947087a441b550
yhunio24 <yhunio62@gmail.com>
2017-02-27 09:21:59 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+         String nama = "yhunio";
+         String kota = "jepara";
+        System.out.println("Nama :" + nama + " Kota :" + kota);
+     }
+}
